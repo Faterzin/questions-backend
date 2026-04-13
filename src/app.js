762b33bc13afd65dccaef70c648d3591d0fc4899
Rafael Nascimento <85128772+Faterzin@users.noreply.github.com>
2026-04-13 09:18:31 -0300

    Add CORS middleware, rename package, remove frontend
    - Install cors package and add app.use(cors()) to allow requests from any origin
    - Rename package from questions-api to questions-backend
    - Remove web/ folder (moved to separate questions-frontend repo)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const questionsRouter = require('./routes/questions')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
